a35f0003903a1ffd7e1fb73d8abe4f17afdf7946
Rachel Pizane <rachel.pzm@gmail.com>
2026-08-23 19:04:46 -0300

    feat: corrigir estilo do painel de login

--- src/shared/components/layout/painel/Painel.tsx
+++ src/shared/components/layout/painel/Painel.tsx
@@ -7,7 +7,7 @@ export function Painel({ className, ...props }: PainelProps) {
   return (
     <section
       className={cn(
-        "m-5 max-h-fit max-w-fit rounded-2xl border-2 border-border bg-card px-6 py-8 shadow-sm",
+        "m-5 max-h-fit w-full rounded-2xl border-2 border-border bg-card p-10 shadow-sm",
         className
       )}
       {...props}
